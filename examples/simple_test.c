a5308040d9610ec7c7788f320219fbb6aaddb9f5
Brian Jackson <bjack205@gmail.com>
2023-02-27 19:59:36 -0500

    Add more stuff

--- examples/simple_test.c
+++ examples/simple_test.c
@@ -6,9 +6,15 @@
 #include "slap/slap.h"
 
 int main(void) {
-  double data[12];
-  Matrix mat = slap_MatrixFromArray(3,4,data);
-  mat.rows = 0;
-  (void)mat;
+  double data_A[12] = {1,2,3, 4,5,6, 7,8,9, 10,11,12};
+  Matrix A = slap_MatrixFromArray(3, 4, data_A);
+  // Get the sizes of the matrix
+  int n_rows = slap_NumRows(A);
+  int n_cols = slap_NumCols(A);
+  int n_el = slap_NumElements(A);
+
+  // Get a pointer to an element
+  double *pa = slap_GetElement(A, 0, 0);
+  double a = *slap_GetElement(A, 0, 1);
   return 0;
 }
