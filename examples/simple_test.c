63239356ba550d66379bc0a904b3cf02786ab339
Brian Jackson <bjack205@gmail.com>
2023-02-27 20:19:39 -0500

    Add unary

--- examples/simple_test.c
+++ examples/simple_test.c
@@ -31,11 +31,49 @@ int main(void) {
   // Set an element directly
   slap_SetElement(A, 1, 2, 25.5);
   printf("A[1,2] = %0.3g (after set)\n", *slap_GetElement(A, 1, 2));
-  
+
   printf("\n~~~~~~~~~~~~~~~ UNARY OPS ~~~~~~~~~~~~~~\n");
 
   // Printing
   printf("\nA:\n");
+  slap_PrintMatrix(A);
+
+  // Set the matrix to a constant
+  const double val = 1.2;
+  slap_SetConst(A, val);
+  printf("\nA: (set const)\n");
+  slap_PrintMatrix(A);
+
+  // Scale by constant
+  slap_ScaleByConst(A, -1);
+  printf("\nA: (Scale by const)\n");
+  slap_PrintMatrix(A);
+
+  // Set Identity
+  slap_SetIdentity(A, 2.1);
+  printf("\nA: (Set to identity)\n");
+  slap_PrintMatrix(A);
+
+  // Set Diagonal
+  double diag[3] = {1,2,3};
+  slap_SetDiagonal(A, diag, 3);
+  printf("\nA: (Set Diagonal)\n");
+  slap_PrintMatrix(A);
+
+  // Set first n elements of diagonal
+  diag[0] = 10;
+  diag[1] = 11;
+  slap_SetDiagonal(A, diag, 2);
+  printf("\nA: (Set Partial Diagonal)\n");
+  slap_PrintMatrix(A);
+
+  // Set Range
+  slap_SetRange(A, 1, slap_NumElements(A));
+  printf("\nA: (Set Range)\n");
+  slap_PrintMatrix(A);
+
+  slap_SetRange(A, 0, 1);
+  printf("\nA: (Set Range 0 to 1)\n");
   slap_PrintMatrix(A);
 
   return 0;
